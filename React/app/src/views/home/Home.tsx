f069b9addc2b1995b2ef04de4e7fe9ea76e969c3
Ryszard Sudujko <73278237+Yellow-Magic-Duck-of-War@users.noreply.github.com>
2021-05-18 16:47:24 +0200

    Aktualizacja: Wykład 4

--- React/app/src/views/home/Home.tsx
+++ React/app/src/views/home/Home.tsx
@@ -2,8 +2,39 @@ import React from 'react';
 
 import NavPanel from '../../components/NavPanel';
 
+import { omdAPIKey } from '../../key';
+
+import http from '../../utils/http'
+
+
 const Home = () =>
 {
+    const [search, setSearch] = React.useState('harry potter');
+    const [movie, setMovie] = React.useState({});
+
+    React.useEffect(() => 
+    {
+        const makeRequest = async () =>
+        {
+            try
+            {
+                const response = await <any>http.get('http://www.omdbapi.com/',
+                {
+                    apikey: omdAPIKey,
+                    s: search,
+                    plot: 'full',
+                });
+                setMovie(response);
+            }
+            catch (error)
+            {
+                console.log(error);
+            }
+
+        };
+        makeRequest();
+    }, [search]);
+    
     return (
         <div>
             <NavPanel />
