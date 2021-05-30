19a6fc738308e31daf7e8fdc8b981c4f92872de1
Ryszard Sudujko <73278237+Yellow-Magic-Duck-of-War@users.noreply.github.com>
2021-05-30 21:01:02 +0200

    Aktualizacja: Wykład 6

---
+++ React/app/src/hooks/useAction.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+import { getAction } from '../store/actions';
+
+export const useAction = <T extends any>(Action: new () => T) =>
+{
+    const { current } = React.useRef(getAction(Action));
+    return current;
+}
